1ec2f02b9896dc10d5c7b197c5406640083b48d3
Christopher Wolfe <noone234@gmail.com>
2023-11-22 17:27:26 -0500

    feat: API has users resource
    The request handlers attempt to interact with a Postgres DB that does
    not exist.  LogRocket's tutorial asks the reader to install Postgres
    server and manually create the DB and tables.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-import Express from "express";
+const express = require("express")
+const db = require("./queries")
 
-const app = Express();
+const app = express();
 const port = 3000;
 
-app.use(Express.json());
-app.use(Express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => console.log("listening on port " + port));
